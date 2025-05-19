eaf8b961a4cf28da39e4bf3452d2d683892cb582
Hayden Amazon <167032968+haydenamazon@users.noreply.github.com>
2025-05-19 12:07:10 -0400

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -12,7 +12,7 @@ def pixel(uid):
     log_entry = f"[{datetime.now()}] UID: {uid}, IP: {request.remote_addr}, Agent: {request.headers.get('User-Agent')}"
     print(log_entry)
     logging.info(log_entry)
-    return send_file('1x1.png', mimetype='image/png')
+    return send_file('a.png', mimetype='image/png')
 
 @app.route('/')
 def home():
